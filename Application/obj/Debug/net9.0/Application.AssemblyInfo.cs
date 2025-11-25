92bc861c23d9a8a8973220cf016e7ef70777d0d7
ddarlaa <d.pugina@list.ru>
2025-11-25 14:35:05 +0300

    Remove sensitive and build files from repository

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9b0e87def402eb6d442e05f74fda603c4b1799")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c70ce8175f95f5f16560fdb2082085ec7a8323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
